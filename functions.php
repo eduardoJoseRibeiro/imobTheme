7dba709a41794598b30f54627d4cca24412d51b1
Eduardo Ribeiro <joseribeiroejrs@ufu.br>
2017-12-13 22:02:45 -0200

    Subindo Single.php e novo função Agora tem pagina separada para cada Imovel

--- functions.php
+++ functions.php
@@ -1,3 +1,47 @@
 <?php
 
 add_theme_support('post-thumbnails');
+
+function cadastrando_post_type_imoveis(){
+    $nome = "Imóvel";
+    $nomePlural = "Imóveis";
+    $description = "Imóveis da Imobiliária";
+
+    $labels = array(
+        "name"          => $nomePlural,
+        "name_singular" => $nome,
+        "add_new_item"  => "Adicionar Novo " . $nome,
+        "edit_item"     => "Editar " . $nome,
+        "new_item"      => "Novo " . $nome,
+        "view_item"     => "Ver " . $nome,
+        "view_items"    => "Ver Novos " . $nomePlural,
+        "search_items"  => "Procurar por " . $nome,
+        "not_found"     => "Nenhum ".$nome." Encontrado",
+        "not_found_in_trash" => "Nenhum ".$nome." na Lixeira",
+        "all_items"     => "Todos os " . $nomePlural,
+        "archives"      => $nomePlural." Arquivados",
+        "attributes"    => "Atributos do " . $nome,
+        "insert_into_item" => "Inserir " . $nome,
+        "uploaded_to_this_item" => "Subir para esse " . $nome,
+        "featured_image" => "http://localhost:8080/wp-content/uploads/2017/12/apartamento2.jpg"
+    );
+
+    $supports = array(
+        "title",
+        "editor",
+        "thumbnail"
+    );
+
+    $args = array(
+        "labels" => $labels,
+        "public" => true,
+        "description" => $description,
+        "menu_icon" => "dashicons-admin-multisite",
+        "supports" => $supports
+    );
+
+    register_post_type('imovel', $args);
+
+}
+
+add_action('init', 'cadastrando_post_type_imoveis');
--- index.php
+++ index.php
@@ -5,14 +5,20 @@
         <h1>O Thema deu certo!</h1>
         <ul class="imoveis-listagem">
             <?php
-                if( have_posts() ) {
-                    while (have_posts()) {
-                        the_post();
+                $args = array(
+                        "post_type" => "imovel"
+                );
+                $loop = new WP_Query($args);
+                if( $loop->have_posts() ) {
+                    while ($loop->have_posts()) {
+                        $loop->the_post();
             ?>
             <li class="imoveis-listagem-item">
-                <?php the_post_thumbnail()?>
-                <h2><?php the_title(); ?></h2>
-                <p><?php the_content();}}?></p>
+                <a href="<?php the_permalink();?>">
+                    <?php the_post_thumbnail()?>
+                    <h2><?php the_title(); ?></h2>
+                    <p><?php the_content();}}?></p>
+                </a>
             </li>
         </ul>
     </div>
